114c2a49469331198f797082f8f405f79aeaaf93
Kyle <kyleghchu@gmail.com>
2023-12-25 13:49:53 -0500

    frontendcode

---
+++ frontend/LatteLink/LatteLink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LatteLink
+//
+//  Created by Kyle Chu on 12/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
